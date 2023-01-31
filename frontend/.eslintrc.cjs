2409482001efb7de482f190ea65a73f30c0b9f80
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-31 17:56:31 -0500

    feat: signup page

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -26,6 +26,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/comma-dangle': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'react/no-children-prop': 'off',
     'operator-linebreak': 'off',
     'object-curly-newline': 'off',
     'prettier/prettier': [
