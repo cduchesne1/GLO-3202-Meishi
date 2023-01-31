38bcfcd3d6e5e30bdb4124cc2a9bf95c50405ca9
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-31 15:14:16 -0500

    feat: home page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home';
+import theme from './theme/theme';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
