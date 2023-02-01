1e19fcabfb0f52f9042e52a878a43ae8dc86a6e2
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-31 20:42:10 -0500

    chore: setup cors, helmet and rate limiting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://meishi.social'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
