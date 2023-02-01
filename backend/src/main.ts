8641512e9a4172f0f979e438d88335c0dcdfc951
Christophe Duchesne <cduchesne.1@gmail.com>
2023-01-31 21:00:03 -0500

    feat: setup config service on backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
@@ -15,6 +17,6 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  await app.listen(parseInt(configService.get('PORT'), 10) || 3000);
 }
 bootstrap();
